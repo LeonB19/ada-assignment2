f6dc98affd74dfbfcbc53bc8fda3c279d2a7c27a
LeonB19 <l.j.t.boeren@student.tue.nl>
2026-05-19 21:24:49 +0200

    refactor: remove API Gateway and related IAM configuration from infrastructure

--- infra/main.tf
+++ infra/main.tf
@@ -136,60 +136,6 @@ resource "google_secret_manager_secret_version" "jwt_secret_value" {
   secret_data = var.jwt_secret
 }
 
-# Cloud Run: API Gateway
-resource "google_cloud_run_v2_service" "api_gateway" {
-  name     = "api-gateway"
-  location = var.region
-
-  template {
-    service_account = google_service_account.runtime_sa.email
-
-    containers {
-      image = "${var.region}-docker.pkg.dev/${var.project_id}/vacation-system/api-gateway:latest"
-
-      env {
-        name  = "PROJECT_ID"
-        value = var.project_id
-      }
-      env {
-        name  = "COORDINATION_AGENT_URL"
-        value = var.coordination_agent_url
-      }
-      env {
-        name  = "BUSINESS_RULES_URL"
-        value = var.business_rules_url
-      }
-      env {
-        name  = "VACATION_REQUEST_URL"
-        value = var.vacation_request_url
-      }
-      env {
-        name = "JWT_SECRET"
-        value_source {
-          secret_key_ref {
-            secret  = google_secret_manager_secret.jwt_secret.secret_id
-            version = "latest"
-          }
-        }
-      }
-    }
-  }
-
-  depends_on = [
-    google_project_service.apis,
-    google_artifact_registry_repository.main,
-  ]
-}
-
-# Allow unauthenticated access to the API Gateway (public entry point)
-resource "google_cloud_run_v2_service_iam_member" "api_gateway_public" {
-  project  = var.project_id
-  location = var.region
-  name     = google_cloud_run_v2_service.api_gateway.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
 # Google Workflow 
 resource "google_workflows_workflow" "package_assembly" {
   name            = "vacation-package-assembly"
